3855fe9f0d28c93c6073b92cd8e5acee62abd08c
David Powell <davidjpowell0@gmail.com>
2020-12-01 22:20:57 +0000

    Fix bad dir

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const readline = require('readline').createInterface({
   output: process.stdout
 });
 
-readline.question('Pick a Day!', async (day: string) => {
+readline.question('Pick a Day!\n', async (day: string) => {
   const chosenDay = parseInt(day.replace(/[^0-9]/g, ''));
   if (Number.isNaN(chosenDay) || chosenDay > 25 || chosenDay <= 0) {
     console.log('Please enter a number from 1-25\n');
@@ -13,7 +13,7 @@ readline.question('Pick a Day!', async (day: string) => {
     return;
   }
   try {
-    const pickedDay = require(`./day${chosenDay}`);
+    const pickedDay = require(`./day${chosenDay}/day${chosenDay}`);
     const input = await fs.readFileSync(path.resolve(__dirname, `./day${chosenDay}/input.txt`), "utf8");
     console.log(`Result:\n\n${pickedDay.run(input)}`);
   } catch (e) {
