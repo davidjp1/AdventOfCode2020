d14855b7b51d27b604ea50b937b75e6905b9eebe
David Powell <davidjpowell0@gmail.com>
2020-12-01 22:17:49 +0000

    day1 p2

--- src/day1.ts
+++ src/day1.ts
@@ -1,9 +1,12 @@
 const run = (input: string) => {
   const inputArr = input.split('\n').map(a => parseInt(a));
-  for(const i of inputArr){
-    for(const j of inputArr){
-      if(i + j === 2020){
-        return i * j;
+  // A very trivial and slow solution to the problem (filters provide a slight optimisation)
+  for (const i of inputArr.filter(a => a < 2020)) {
+    for (const j of inputArr.filter(a => i + a < 2020)) {
+      for (const k of inputArr) {
+        if (i + j + k === 2020) {
+          return i * j * k;
+        }
       }
     }
   }
