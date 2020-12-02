fa09c388bedc067ed0d314f2cfbc223977379889
David Powell <davidjpowell0@gmail.com>
2020-12-02 08:41:42 +0000

    day2 p2

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -15,15 +15,29 @@ const toPasswordObject = (line: string): PasswordPolicy => {
   }
 }
 
-const validatePassword = ({minOccurences, maxOccurences, character, password}: PasswordPolicy) => {
+const validatePasswordPart1 = ({minOccurences, maxOccurences, character, password}: PasswordPolicy) => {
   const characterCount = (password.match(new RegExp(character, 'g')) || []).length;
   return characterCount >= minOccurences && characterCount <= maxOccurences;
+}
+
+const validatePasswordPart2 = ({minOccurences, maxOccurences, character, password}: PasswordPolicy) => {
+  const indexOfFirstChar = minOccurences - 1;
+  const indexOfSecondChar = maxOccurences - 1;
+  let matches = 0;
+  if(password[indexOfFirstChar] === character){
+    matches++;
+  }
+  if(password[indexOfSecondChar] === character){
+    matches++;
+  }
+  console.log(password, password[indexOfFirstChar], password[indexOfSecondChar], matches);
+  return matches === 1;
 }
 
 const run = (input: string): number => {
   const document = input.split('\n');
   const passwords = document.map(toPasswordObject);
-  return passwords.filter(validatePassword).length;
+  return passwords.filter(validatePasswordPart2).length;
 };
 
 export { run };
