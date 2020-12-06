dc7db10fc45f887cf5e7def761eb2a5690ae894c
David Powell <davidjpowell0@gmail.com>
2020-12-06 12:00:40 +0000

    day6 p2

--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,8 +1,18 @@
+import { format } from "path";
+
 const run = (input: string): number => {
   const getAnsweredQuestionCount = (form: string[]): number => {
-    const uniqueAnswers = new Set();
-    form.forEach(personAnswers => personAnswers.split('').forEach(answer => uniqueAnswers.add(answer)));
-    return uniqueAnswers.size;
+    const numberOfPeople = form.length;
+    // A dict mappings of question answered to number of occurrences
+    const answersDict: { [question: string]: number } = {};
+    form.forEach(personsAnswers => personsAnswers.split('').forEach(answer => {
+      if(!answersDict[answer]){
+        answersDict[answer] = 1;
+      } else {
+        answersDict[answer]++;
+      }
+    }));
+    return Object.entries(answersDict).filter(([_, count]) => count === numberOfPeople).length;
   };
 
   const questionAnswers: string[][] = input.replace(/\r/g, '').split('\n\n').map(form => form.split('\n'));
