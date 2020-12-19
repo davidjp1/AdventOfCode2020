e477b322f155514142cd004f2ab6852576bd7460
David Powell <davidjpowell0@gmail.com>
2020-12-19 13:24:03 +0000

    day12 p2

--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -10,7 +10,7 @@ enum Direction {
   // Direction to move forwards in currently facing direction
   FORWARD = 'F'
 }
-interface BoatState {
+interface State {
   // Number of degrees (0 === NORTH, 90 === EAST etc.)
   facingDirection: number,
   xPos: number,
@@ -31,7 +31,7 @@ const degreesToDirection = (degrees: number): Direction => {
   }
 }
 
-const doAction = (boatState: BoatState, action: Direction, amount: number) => {
+const doAction = (boatState: State, action: Direction, amount: number): void => {
   switch (action) {
     case Direction.NORTH:
       boatState.yPos += amount;
@@ -63,17 +63,65 @@ const doAction = (boatState: BoatState, action: Direction, amount: number) => {
   }
 }
 
+const rotateWaypoint = (waypoint: State, clockwise: boolean, amount: number): void => {
+  const radians = Math.PI * (clockwise ? amount : -amount) / 180;
+
+  const newX = Math.cos(radians) * waypoint.xPos + Math.sin(radians) * waypoint.yPos;
+  const newY = Math.cos(radians) * waypoint.yPos - Math.sin(radians) * waypoint.xPos;
+  waypoint.xPos = Math.round(newX);
+  waypoint.yPos = Math.round(newY);
+}
+
+const doActionWithWaypoint = (boatState: State, waypointState: State, action: Direction, amount: number): void => {
+  switch (action) {
+    case Direction.NORTH:
+      waypointState.yPos += amount;
+      break;
+    case Direction.EAST:
+      waypointState.xPos += amount;
+      break;
+    case Direction.SOUTH:
+      waypointState.yPos -= amount;
+      break;
+    case Direction.WEST:
+      waypointState.xPos -= amount;
+      break;
+    case Direction.LEFT:
+      rotateWaypoint(waypointState, false, amount);
+      break;
+    case Direction.RIGHT:
+      rotateWaypoint(waypointState, true, amount);
+      break;
+    case Direction.FORWARD:
+      boatState.xPos += waypointState.xPos * amount;
+      boatState.yPos += waypointState.yPos * amount;
+      break;
+  }
+}
+
 const run = (input: string): number => {
   const actions = input.replace(/\r/g, '').split('\n').map(a => ({
     action: a[0] as Direction,
     amount: parseInt(a.substring(1, a.length))
   }));
-  const state: BoatState = {
+  const state: State = {
     facingDirection: 90,
     xPos: 0,
     yPos: 0
   }
   actions.forEach(({ action, amount }) => doAction(state, action, amount));
+  console.log(`Part 1 = ` + (Math.abs(state.xPos) + Math.abs(state.yPos)));
+
+  const waypointState = {
+    xPos: 10,
+    yPos: 1,
+    facingDirection: 0
+  }
+  //Reset boat state (facing direction not used in p2)
+  state.xPos = 0;
+  state.yPos = 0;
+  actions.forEach(({action, amount}) => doActionWithWaypoint(state, waypointState, action, amount));
   return Math.abs(state.xPos) + Math.abs(state.yPos);
+
 }
 export { run }
