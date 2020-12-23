960a07c796e3182093562197950d70e35759ff4c
David Powell <davidjpowell0@gmail.com>
2020-12-23 23:06:52 +0000

    day13 p2

--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-constant-condition */
 const getFirstAvaiableBusWait = (bus: number, earliestTime: number) : number => {
   let current = 0;
   while(current < earliestTime){
@@ -6,12 +7,40 @@ const getFirstAvaiableBusWait = (bus: number, earliestTime: number) : number => 
   return current - earliestTime;
 }
 
-const run = (input: string): number => {
-  const inputArr = input.replace(/\r/g, '').split('\n');
-  const earliestTime = parseInt(inputArr[0]);
-  const busTimers = inputArr[1].split(',').filter(a => a !== 'x').map(a => parseInt(a));
+const part1 = (input: string[]): number => {
+  const earliestTime = parseInt(input[0]);
+  const busTimers = input[1].split(',').filter(a => a !== 'x').map(a => parseInt(a));
   const bestBus = busTimers.map(bus => ({busId: bus, timeToWait: getFirstAvaiableBusWait(bus, earliestTime)}))
     .sort((a,b) => a.timeToWait - b.timeToWait)[0];
+
   return bestBus.busId * bestBus.timeToWait;
+}
+const part2 = (inputArr: string[]): number => {
+  const buses: number[] = [];
+  inputArr[1].split(',').forEach((bus,i) => {
+    if(bus !== 'x'){
+      buses[i] = parseInt(bus);
+    }
+  });
+
+  let t = 0;
+  let offset = 1;
+  // https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+  buses.forEach((bus, index) => {
+    while (true) {
+      if ((t + index) % bus === 0) {
+        offset = offset * bus;
+        break;
+      }
+      t += offset;
+    }
+  });
+  return t;
+}
+
+const run = (input: string): number => {
+  const inputArr = input.replace(/\r/g, '').split('\n');
+  console.log(`Part 1: `+part1(inputArr));
+  return part2(inputArr);
 }
 export { run }
